b7fc3fd8d482c5bb0d5f9b035aacb42f61141458
EmileCar <caron.emile@telenet.be>
2024-09-10 21:10:11 +0200

    Refactor Button component to use styled-components

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Button.css';
+import styled from 'styled-components';
 import LoadingSpinner from '../loading/LoadingSpinner';
 import { classNames } from '../../utils/classNameUtil';
 
@@ -19,50 +19,70 @@ interface ButtonProps {
     pending?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({
-    text,
-    onClick,
-    fullWidth = false,
-    disabled = false,
-    uppercase = false,
-    customClassName = '',
-    hover = false,
-    darken = false,
-    round = false,
-    inverted = false,
-    submit = false,
-    icon,
-    pending = false,
-}) => {
-    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
-        onClick();
-    };
+const StyledButton = styled.button<ButtonProps>`
+  background-color: var(--main-color);
+  padding: var(--s0);
+  border-radius: ${(props) => (props.round ? '50px' : 'var(--border-radius-md)')};
+  color: white;
+  text-decoration: none;
+  transition: transform 0.1s ease-in-out, filter 0.1s ease-in-out;
+  display: inline-block;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  text-align: center;
+  min-height: 55px;
+  box-shadow: 1px 0px 15px rgb(197, 197, 197);
+  width: ${(props) => (props.fullWidth ? '100%' : props.submit ? '50%' : 'auto')};
+  margin-top: ${(props) => (props.submit ? 'var(--s1)' : '0')};
+  background-color: ${(props) => (props.inverted ? 'white' : 'var(--main-color)')};
+  color: ${(props) => (props.inverted ? 'var(--main-color)' : 'white')};
+  font-weight: ${(props) => (props.uppercase ? 'bold' : 'normal')};
+  text-transform: ${(props) => (props.uppercase ? 'uppercase' : 'none')};
 
-    return (
-        <button
-            onClick={handleClick}
-            className={classNames(
-                'button inherit-font',
-                customClassName,
-                hover && 'hover',
-                darken && 'darken',
-                uppercase && 'uppercase',
-                inverted && 'inverted',
-                submit && 'submit',
-                fullWidth && 'full-width'
-            )}
-            disabled={disabled}
-            style={{ borderRadius: round ? '50px' : undefined }}
-        >
-            {pending ? <LoadingSpinner color='white' size={22}/> : (
-                <>
-                    {icon && <span className={`pi ${icon}`}></span>}
-                    {text}
-                </>
-            )}
-        </button>
-    );
+  &:hover {
+    transform: ${(props) => (props.hover ? 'scale(1.05)' : 'none')};
+    filter: ${(props) => (props.darken ? 'brightness(0.8)' : 'none')};
+  }
+`;
+
+const Button: React.FC<ButtonProps> = ({
+  text,
+  onClick,
+  fullWidth = false,
+  disabled = false,
+  uppercase = false,
+  customClassName = '',
+  hover = false,
+  darken = false,
+  round = false,
+  inverted = false,
+  submit = false,
+  icon,
+  pending = false,
+}) => {
+
+  return (
+    <StyledButton
+      onClick={onClick}
+      fullWidth={fullWidth}
+      disabled={disabled}
+      uppercase={uppercase}
+      hover={hover}
+      darken={darken}
+      round={round}
+      inverted={inverted}
+      submit={submit}
+      className={customClassName}
+    >
+      {pending ? <LoadingSpinner color='white' size={22}/> : (
+        <>
+          {icon && <span className={`pi ${icon}`}></span>}
+          {text}
+        </>
+      )}
+    </StyledButton>
+  );
 };
 
 export default Button;
--- src/components/loading/LoadingSpinner.tsx
+++ src/components/loading/LoadingSpinner.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./Spinner.css";
 
 interface LoadingSpinnerProps {
   color?: string;
