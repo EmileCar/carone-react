b2a8222b11e42f1b5a88613c43cf9a9cdc7ece39
EmileCar <caron.emile@telenet.be>
2024-09-12 15:56:24 +0200

    Refactor Button component props and styles

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -4,35 +4,39 @@ import LoadingSpinner from '../loading/LoadingSpinner';
 import { classNames } from '../../utils/classNameUtil';
 
 interface ButtonProps {
+    /** The text to display on the button */
     text?: string;
-    onClick: () => void;
-    fullWidth?: boolean;
-    disabled?: boolean;
+    /** If the text should be uppercase */
     uppercase?: boolean;
+    /** The callback function to call when the button is clicked */
+    onClick: () => void;
+    /** If the button should take up the full width of its container */
+    fullWidth?: boolean;
+    /** If the button should be disabled */
+    disabled?: boolean;
+    /** A custom class name to apply to the button */
     className?: string;
-    hover?: boolean;
-    darken?: boolean;
-    round?: boolean;
-    inverted?: boolean;
-    submit?: boolean;
+    /** If the button should have one or more hover effects */
+    hoverEffect?: string;
+    /** The amount of px to make the button round */
+    borderRadius?: number;
+    /** The icon of a button. This will a put before the text. */
     icon?: string;
+    /** If the state is pending, a loading icon will be shown instead of the text. */
     pending?: boolean;
 }
 
 const Button: React.FC<ButtonProps> = ({
-    text,
+    text = "Default button",
+    uppercase = false,
     onClick,
     fullWidth = false,
     disabled = false,
-    uppercase = false,
     className = '',
-    hover = false,
-    darken = false,
-    round = false,
-    inverted = false,
-    submit = false,
-    icon,
-    pending = false,
+    hoverEffect = '',
+    borderRadius = 0,
+    icon = '',
+    pending= false
 }) => {
     const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
@@ -45,21 +49,20 @@ const Button: React.FC<ButtonProps> = ({
             className={classNames(
                 'carone-button inherit-font',
                 className,
-                hover && 'hover',
-                darken && 'darken',
-                uppercase && 'uppercase',
-                inverted && 'inverted',
-                submit && 'submit',
-                fullWidth && 'full-width'
+                hoverEffect,
             )}
             disabled={disabled}
-            style={{ borderRadius: round ? '50px' : undefined }}
+            style={{
+                ...(borderRadius && { borderRadius: `${borderRadius}px` }),
+                ...(fullWidth && { width: '100%' }),
+                ...(uppercase && { textTransform: 'uppercase' }),
+            }}
         >
             {pending ? <LoadingSpinner color='white' size={22}/> : (
-                <>
-                    {icon && <span className={`pi ${icon}`}></span>}
-                    {text}
-                </>
+            <>
+                {icon && <span className={`pi ${icon}`}></span>}
+                {text}
+            </>
             )}
         </button>
     );
