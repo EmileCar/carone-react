6cb37e6e9ca7d146c59e803a3cd5f3156094ae7e
EmileCar <caron.emile@telenet.be>
2024-09-17 10:11:10 +0200

    Refactor Footer component to remove unused code

--- src/components/page/footer/Footer.tsx
+++ src/components/page/footer/Footer.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { useBanner } from '../../../contexts/BannerContext';
 
 /**
  * The footer of the application.
  * This component should be used inside a Page component.
 */
 const Footer: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    const { setBanner } = useBanner();
 
     return (
         <footer className="carone-footer">
