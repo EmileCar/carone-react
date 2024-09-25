66ad90503ae9d48d1caeb208eed946be9e996092
EmileCar <caron.emile@telenet.be>
2024-09-25 18:14:49 +0200

    Hero bug fixes

--- src/components/hero/Hero.stories.tsx
+++ src/components/hero/Hero.stories.tsx
@@ -12,14 +12,13 @@ export default {
 };
 
 const heroSliderProps = {
-    imagePaths: [
-        'https://ksaoosterzele.be/assets/hero/stellingstartdag.jpg',
-        'https://ksaoosterzele.be/assets/hero/kampsfeer.jpg',
-        'https://via.placeholder.com/1920x1080',
+    images: [
+        { path: 'https://ksaoosterzele.be/assets/hero/stellingstartdag.jpg', alt: 'Stelling Startdag' },
+        { path: 'https://ksaoosterzele.be/assets/hero/kampsfeer.jpg', alt: 'Kampsfeer' },
+        { path: 'https://via.placeholder.com/1920x1080', alt: 'Placeholder' },
     ],
     interval: 5000,
     altText: 'Hero image',
-    blurredImagePath: 'https://ksaoosterzele.be/assets/hero/blur.jpg',
 };
 
 export const SimpleHero = () => (
--- src/components/hero/HeroSlider.tsx
+++ src/components/hero/HeroSlider.tsx
@@ -1,40 +1,60 @@
 import React, { useState, useEffect, useMemo } from "react";
 import "../../styles/Hero.css";
 
-export interface HeroSliderProps {
-    imagePaths?: string[];
-    interval?: number;
-    altText?: string;
-    blurredImagePath?: string;
+/**
+ * The props for each image in the HeroSlider component
+ */
+export interface HeroSliderImageProps {
+    /** The path to the image */
+    path: string;
+    /** The alt text for the image */
+    alt?: string;
 }
 
+/*
+ * The props for the HeroSlider component
+ */
+export interface HeroSliderProps {
+    /** The array of images to display in the slider. If there is only one image, it will be displayed without a slider */
+    images: HeroSliderImageProps[];
+    /** The interval in milliseconds between image transitions */
+    interval?: number;
+    /** The path to the blurred image */
+    blurredImagePath?: HeroSliderImageProps;
+}
+
+/**
+ * A hero slider component that can be customized with different props.
+ *
+ * @param {HeroSliderProps} props the props for the component
+ * @returns {React.ReactElement} the hero slider component
+ */
 const HeroSlider: React.FC<HeroSliderProps> = ({
-    imagePaths = [],
+    images = [],
     interval = 7000,
-    altText = "Hero image",
-    blurredImagePath = "hero-blurred",
+    blurredImagePath,
 }) => {
     const [activeImageIndex, setActiveImageIndex] = useState<number>(0);
-    const [loadedImages, setLoadedImages] = useState<boolean[]>(Array(imagePaths.length).fill(false));
+    const [loadedImages, setLoadedImages] = useState<boolean[]>(Array(images.length).fill(false));
     const [allImagesLoaded, setAllImagesLoaded] = useState<boolean>(false);
 
     useEffect(() => {
-        if(!allImagesLoaded) return;
+        if(!allImagesLoaded || images.length <= 1) return;
         const effectiveInterval = Math.max(interval, 1000);
         const intervalObj = setInterval(() => {
-            setActiveImageIndex((prevIndex) => (prevIndex + 1) % imagePaths.length);
+            setActiveImageIndex((prevIndex) => (prevIndex + 1) % images.length);
         }, effectiveInterval);
 
         return () => {
             clearInterval(intervalObj);
         };
-    }, [imagePaths.length, allImagesLoaded]);
+    }, [images.length, allImagesLoaded]);
 
     useEffect(() => {
         const loadImages = () => {
-            imagePaths.forEach((imagePath, index) => {
+            images.forEach((image, index) => {
                 const img = new Image();
-                img.src = imagePath;
+                img.src = image.path;
                 img.onload = () => {
                     setLoadedImages((prev) => {
                         const updated = [...prev];
@@ -49,29 +69,29 @@ const HeroSlider: React.FC<HeroSliderProps> = ({
         };
 
         loadImages();
-    }, [imagePaths]);
+    }, [images]);
 
     return (
         <div className="carone-hero-images layered-grid">
-            {imagePaths.map((imagePath, index) => (
+            {images.map((image, index) => (
                 <div
-                    key={imagePath}
+                    key={image.alt}
                     className={`carone-hero-images__image ${
                         index === activeImageIndex ? "carone-hero-images__image--active" : ""
                     }`}
                 >
                     <img
-                        src={imagePath}
-                        alt={altText}
+                        src={image.path}
+                        alt={image.alt}
                         style={{ display: loadedImages[index] ? "block" : "none" }}
                     />
                 </div>
             ))}
-            {!allImagesLoaded && (
-                <div className="hero__image active">
+            {(blurredImagePath && !allImagesLoaded) && (
+                <div className="carone-hero-images__image carone-hero-images__image--active">
                     <img
-                        src={blurredImagePath}
-                        alt="Hero blurred image"
+                        src={blurredImagePath.path}
+                        alt={blurredImagePath.alt}
                     />
                 </div>
             )}
