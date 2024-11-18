57123b40d141849f9c87693e21c629e09a58cb20
EmileCar <caron.emile@telenet.be>
2024-11-18 19:41:39 +0100

    Add Flex component and associated styles for flexible layout

---
+++ src/components/flex/Flex.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import '../../styles/Page.css';
+import { classNames } from '../../utils/classNameUtil';
+import { Size } from '../../configurations/CaroneConfig';
+
+/**
+ * The props for the Flex component.
+ */
+interface FlexProps {
+    /** The justify content */
+    justifyContent?: 'start' | 'end' | 'center' | 'space-between' | 'space-around' | 'space-evenly';
+    /** The align items */
+    alignItems?: 'start' | 'end' | 'center' | 'stretch' | 'baseline';
+    /** The flex direction */
+    direction?: 'row' | 'column';
+    /** The gap between the flex items */
+    gap?: number | string | Size;
+    /** A custom class name to apply to the flex */
+    className?: string;
+    /** A custom style object to apply to the flex */
+    style?: React.CSSProperties;
+    /** The children components */
+    children: React.ReactNode;
+}
+
+/**
+ * A flex component that can be customized with different props.
+ *
+ * @param {FlexProps} props the props for the component
+ * @returns {React.ReactElement} the flex component
+ */
+const Page: React.FC<FlexProps> = ({
+    justifyContent= 'start',
+    alignItems= 'start',
+    direction= 'row',
+    gap,
+    className= '',
+    style,
+    children,
+}) => {
+    return (
+        <div
+            className={classNames(
+                'carone-flex',
+                className,
+            )}
+            style={{
+                justifyContent: justifyContent,
+                alignItems: alignItems,
+                flexDirection: direction,
+                gap: gap,
+                ...style,
+            }}
+        >
+            {children}
+        </div>
+    );
+};
+
+export default Page;
